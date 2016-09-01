fe066223474f3821a448bdd7b5e1297c8c876391
Ira Demkovych <ira.demkovych@gmail.com>
2016-09-01 13:48:51 +0000

    Initial2

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 var num =  Number(prompt('Enter number'));
 
-// function fib (n){
-//     var a = 1;
-//     var b = 1;
-//      for (var i = 3; i<=n; i++){
-//      var c = a + b;
-//      a = b;
-//      b = c;
-//     }
-//      return b;
-// }
-//     alert(fib(num)); 
-    
-function fib(n) {
-    if (n<=1){
-        return n
-    } else {
-        return fib(n - 1) + fib(n - 2);
+function fib (n){
+    var a = 1;
+    var b = 1;
+     for (var i = 3; i<=n; i++){
+     var c = a + b;
+     a = b;
+     b = c;
     }
+     return b;
 }
+    alert(fib(num)); 
+    
+// function fib(n) {
+//     if (n<=1){
+//         return n
+//     } else {
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// }
 
-alert(fib(num));
+// alert(fib(num));
